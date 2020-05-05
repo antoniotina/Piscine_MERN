043d9eb409f7acc76bb32ac80b1d88db01555810
unknown <antonioarantestina@gmail.com>
2020-05-05 22:50:32 +0200

    Ex 05 and 06 done

--- Piscine_MERN_Jour_02/ex_05/server.js
+++ Piscine_MERN_Jour_02/ex_05/server.js
@@ -17,17 +17,31 @@ const port = process.env.PORT || 4242
 const host = process.env.HOST || 'localhost'
 process.env.NODE_ENV = "development"
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
+    fs.readFile('hello.html', function(err, data) {
+        res.sendFile(__dirname + '/form.html')
+    })
+})
+
+app.post('/insert', (req, res) => {
 
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
         if (err) {
             throw err
         } else {
             let dbo = db.db("mern-pool")
-            var query = { validated: "in progress" };
-            dbo.collection('students').find(query).sort({ lastname: 1 }).toArray(function(err, result) {
-                if (err) throw err;
-                res.send(result);
+            if (req.body.student.admin == 'false') {
+                req.body.student.admin = false
+            } else {
+                req.body.student.admin = true
+            }
+            let collection = dbo.collection('students')
+            dbo.collection('students').insertOne(req.body.student, function(err, resb) {
+                if (err) {
+                    res.send("Failed to save the collection.")
+                } else {
+                    res.send("Collection saved.")
+                }
             });
         }
     });
